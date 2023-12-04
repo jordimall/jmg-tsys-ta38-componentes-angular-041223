0b0eac7cd8a99040e6eedb10df625dc97880275d
jordimall <jordi.mallafre@hotmail.com>
2023-12-04 17:50:24 +0100

    Creación y comprobación del componente formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ClienteComponent } from "./cliente/cliente.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, ClienteComponent]
 })
 export class AppComponent {
   title = 'Componentes';
